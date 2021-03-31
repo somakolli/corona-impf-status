db637d869d0e19d87796fd60beead139e2dc3b07
Sokol Makolli <st116214@stud.uni-stuttgart.de>
2021-03-31 23:38:42 +0200

    remove light font

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,6 @@ import CopyPlugin from 'copy-webpack-plugin'
 
 export default {
     mode: 'production',
-    module: {
-      rules: [{
-          test: /\.txt$/i,
-          loader: 'raw-loader'
-      }]
-    },
     plugins: [
         new CopyPlugin({
             patterns: [
